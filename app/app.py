68172983a9daf67ca69c1e0cf83fc0b63daea61f
csantosd <celia.santos@sonicat-systems.com>
2021-03-21 10:36:32 +0100

    updated code to set up workdir

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
 import logging
 from pathlib import Path
 from dotenv import load_dotenv
+import sys
 
+workdir = '/home/csantos/src/aidl/video_processor'
+sys.path.insert(0, workdir)
 
-from app.controller.Controller import Controller
 from app.config.RabbitTemplate import RabbitTemplate
-
+from app.controller.Controller import Controller
 
 def load_env():
-    env_path = Path('env') / '.env'
+    env_path = Path(workdir + '/env') / '.env'
     load_dotenv(dotenv_path=env_path)
 
 
@@ -17,7 +19,8 @@ if __name__ == '__main__':
     load_env()
 
     # Create RPC Controller and start consumer
-    controller = Controller()
-    logging.info(" [X] Start Consuming: ")
+    rabbit_template = RabbitTemplate()
+    controller = Controller(rabbit_template)
     controller.start()
-
+    print(" [X] Start Consuming: ")
+    rabbit_template.channel.start_consuming()
--- app/controller/Controller.py
+++ app/controller/Controller.py
@@ -1,16 +1,17 @@
-from app.GesturesAnalyzer.GesturesAnalyzer import GesturesAnalyzer
-from app.config.RabbitTemplate import RabbitTemplate
+import datetime
 
+from app.GesturesAnalyzer.GesturesAnalyzer import GesturesAnalyzer
 
 class Controller:
 
-    def __init__(self):
-        self.rabbit_template = RabbitTemplate()
+    def __init__(self, rabbit_template):
+        self.rabbit_template = rabbit_template
         self.gestures_analyzer = GesturesAnalyzer()
 
     def start(self):
         self.rabbit_template.start_consuming(self.consume_video)
 
     def consume_video(self, ch, method, props, body):
+        print("Video received at: " + str(datetime.datetime.utcnow()))
         response = self.gestures_analyzer.process_video(video=body)
         self.rabbit_template.rpc_reply(ch, method, props, response)
