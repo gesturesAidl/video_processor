5cc628435283d2e5ce0d941e4e8342da66d3f60b
Sofia <sofia_jacques@hotmail.com>
2021-04-05 01:18:14 +0900

    Add process video to Controller

--- app/controller/Controller.py
+++ app/controller/Controller.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import os
 
 from app.GesturesAnalyzer.GesturesAnalyzer import GesturesAnalyzer
 
@@ -10,14 +11,20 @@ class Controller:
         self.rabbit_template = rabbit_template
         self.gestures_analyzer = GesturesAnalyzer()
 
+    
+
+
     def init(self):
         self.rabbit_template.start_consuming(self.consume_video)
 
     def consume_video(self, ch, method, props, body):
         print("Video received at: " + str(datetime.datetime.utcnow()))
         frames = json.loads(body)
-        # TODO: replace .save_video(frames) method by process_video(frames) when its code has been implemented.
-        #  With save_video code, we can check that videos are sent and saved correctly
-        response = self.gestures_analyzer.save_video(frames)
-        # response = self.gestures_analyzer.process_video(video=frames)
+       
+        #TODO: Need to modify. Rabbit expects response before sending next video, but prediction is
+        # only done once every 2 videos. As a workaround, return Gestures() in gestures_analyzer.save_video(frames)
+        response, path = self.gestures_analyzer.save_video(frames)
+        if os.path.exists(path):
+            response = self.gestures_analyzer.process_video(path)
+
         self.rabbit_template.rpc_reply(ch, method, props, response)
